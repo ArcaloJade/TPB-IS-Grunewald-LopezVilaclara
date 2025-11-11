26b705f6c85fe59a28a0dcd1cfa8b737eadacf18
ArcaloJade <mateolv2003@gmail.com>
2025-11-11 17:51:07 -0300

    Commit de SpringBoot

---
+++ src/test/java/org/udesa/GiftcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
